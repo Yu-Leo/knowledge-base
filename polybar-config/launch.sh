dd05e4db210b387e9de7eb2e405ee8d4f9ece1ed
Lev Yuvensky <levayu22@gmail.com>
2022-03-23 20:58:36 +0300

    Add i3 and polybar configs

---
+++ polybar-config/launch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Завершить текущие экземпляры polybar
+killall -q polybar
+
+# Ожидание полного завершения работы процессов
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Запуск Polybar со стандартным расположением конфигурационного файла в ~/.config/polybar/config
+polybar example &
+
+echo "Polybar загрузился..."
